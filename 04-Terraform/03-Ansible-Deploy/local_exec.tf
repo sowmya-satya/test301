1cc44f78661c9bc50b9cf4fe06bfbd34e24d445d
root <root@mtadminnuvepro.w1lwkyb1x53ufgoqvmbrxbhoie.cx.internal.cloudapp.net>
2020-02-02 12:44:39 +0000

    hello3

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -6,12 +6,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   
 }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-04763b3055de4860b"
+  ami                    = "ami-0d03add87774b12c5"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
